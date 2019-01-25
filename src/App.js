8cb5a6d87f67ca9b9f31b64dadd6afcef637e8ab
50victor <victorvcdb@gmail.com>
2019-01-25 11:46:34 -0200

    ada

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import  Menu from './components/menu';
-import Report from './pages/report';
-import Home from './pages/home';
-import StudyMaterial from './pages/studyMaterial';
-import Ranking from './pages/ranking';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Menu from "./components/menu";
+import Report from "./pages/report";
+import Home from "./pages/home";
+import StudyMaterial from "./pages/studyMaterial";
+import Ranking from "./pages/ranking";
+import "./App.css";
 
+const App = () => (
+  <div>
+    <Menu />
 
-class App extends Component {
-  render() {
-    return (
-      
-        
-        <div>
-          <Menu/>
-          <Ranking/>
-          <StudyMaterial/>
-          <Report/>
-          <Home/>
-        </div>
-
-     
-    );
-  }
-}
+    <Route path="/" exact component={Home} />
+    <Route path="/ranking" exact component={Ranking} />
+    <Route path="/report" exact component={Report} />
+    <Route path="/matstudy" exact component={StudyMaterial} />
+  </div>
+);
 
 export default App;
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,73 +1,106 @@
-import React from 'react';
-import { Menu, Icon, Avatar, Badge, Layout } from 'antd';
-
+import React from "react";
+import { Menu, Icon, Avatar, Badge, Layout } from "antd";
+import { Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MenuItemGroup = Menu.ItemGroup;
 const SubMenu = Menu.SubMenu;
 
-
-
 export default class Pag extends React.Component {
-    state = {
-        current: 'mail',
-    }
-    handleClick = (e) => {
-        console.log('click', e);
-        this.setState({
-            current: e.key,
-        });
-    }
-    render() {
-        return (
+  state = {
+    current: "mail"
+  };
+  handleClick = e => {
+    console.log("click", e);
+    this.setState({
+      current: e.key
+    });
+  };
+  render() {
+    return (
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="mail">
+          <Icon type="home" />
+          Início
+          <Link to="./">Início</Link>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <Icon type="trophy" />
+          Ranking
+          <Link to="./Ranking" />
+        </Menu.Item>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="snippets" />
 
-            <Menu 
-                onClick={this.handleClick}
-                selectedKeys={[this.state.current]}
-                mode="horizontal">
-                <Menu.Item key="mail">
-                    <Icon type="home" />Início
-                    </Menu.Item>
-                <Menu.Item key="app">
-                    <Icon type="trophy" />Ranking
-                    </Menu.Item>
-                <SubMenu title={<span className="submenu-title-wrapper"><Icon type="snippets" />Material de Estudos</span>}>
-                    <MenuItemGroup title="Modulo 1">
-                        <Menu.Item key="Modulo:1"> Modulo 1
-                        <Icon type="unlock" />
-                        </Menu.Item>
-                        <Menu.Item key="Questionario:1" disabled> Questionário 1
-                        <Icon type="lock" />
-                        </Menu.Item>
-                    </MenuItemGroup>
+              <Link to="./matstudy">Material de Estudos</Link>
+            </span>
+          }
+        >
+          <MenuItemGroup title="Modulo 1">
+            <Menu.Item key="Modulo:1">
+              {" "}
+              Modulo 1
+              <Icon type="unlock" />
+            </Menu.Item>
+            <Menu.Item key="Questionario:1" disabled>
+              {" "}
+              Questionário 1
+              <Icon type="lock" />
+            </Menu.Item>
+          </MenuItemGroup>
 
-                    <MenuItemGroup title="Modulo 2">
-                        <Menu.Item key="Modulo:2" disabled >Modulo 2
-                        <Icon type="lock" />
-                        </Menu.Item>
-                        <Menu.Item key="Questionario:2" disabled > Questionário 2
-                        <Icon type="lock" />
-                        </Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
+          <MenuItemGroup title="Modulo 2">
+            <Menu.Item key="Modulo:2" disabled>
+              Modulo 2
+              <Icon type="lock" />
+            </Menu.Item>
+            <Menu.Item key="Questionario:2" disabled>
+              {" "}
+              Questionário 2
+              <Icon type="lock" />
+            </Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
 
-                <SubMenu title={<span className="submenu-title-wrapper"><Icon type="area-chart" />Relatório</span>}>
-                    <MenuItemGroup>
-                        <Menu.Item key="Relatorio:1"> Modulo 1
-                        <Icon type="line-chart" />
-                        </Menu.Item>
-                    </MenuItemGroup>
-                    <MenuItemGroup >
-                        <Menu.Item key="Relatorio:2" disabled > Modulo 2  <Icon type="pie-chart" />
-                        </Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="area-chart" />
 
-                <Badge dot> <Avatar icon="mail" /></Badge>
-                <Badge dot> <Avatar style={{ right: '0%' }} icon="user" /></Badge>
+              <Link to="./Report">Relatório</Link>
+            </span>
+          }
+        >
+          <MenuItemGroup>
+            <Menu.Item key="Relatorio:1">
+              {" "}
+              Modulo 1
+              <Icon type="line-chart" />
+            </Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup>
+            <Menu.Item key="Relatorio:2" disabled>
+              {" "}
+              Modulo 2 <Icon type="pie-chart" />
+            </Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
 
-            </Menu>
-
-        )
-    }
-
+        <Badge dot>
+          {" "}
+          <Avatar icon="mail" />
+        </Badge>
+        <Badge dot>
+          {" "}
+          <Avatar style={{ right: "0%" }} icon="user" />
+        </Badge>
+      </Menu>
+    );
+  }
 }
