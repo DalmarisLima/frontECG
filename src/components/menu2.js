03b204da006313e870a7e5e02856b6b09683e586
unknown <moraesdalmarislima@gmail.com>
2019-02-23 23:34:35 -0300

    oitavo commit

--- src/components/menu2.js
+++ src/components/menu2.js
@@ -29,51 +29,69 @@ export default class Primary extends React.Component {
                 <Sider
                     collapsible
                     collapsed={this.state.collapsed}
-                    onCollapse={this.onCollapse}
-                >
-                    <div className="logo" />
+                    onCollapse={this.onCollapse}>
+                    <div style={{ position: 'center' }} >ECG TUTOR</div>
                     <Menu theme="dark" menu-dark-bg='#6CB6C9' defaultSelectedKeys={['1']} mode="inline">
                         <Menu.Item key="1">
                             <Icon type="home" />
                             <span>Início</span>
                             <Link to="./"></Link>
                         </Menu.Item>
-                        <Menu.Item key="8">
-                            <Icon type="user" />
+                        <Menu.Item key="2">
+                            <Icon type="smile" />
                             <span>Perfil</span>
                             <Link to="./" />
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <Icon type="trophy" />
-                                <span>Ranking</span>
-                                <Link to="./Ranking" />
-                            </Menu.Item>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="trophy" />
+                            <span>Ranking</span>
+                            <Link to="./Ranking" />
+                        </Menu.Item>
                         <SubMenu
                             key="sub1"
                             title={<span> <Icon type="snippets" />
                                 <span>Material de Estudos</span>
-                            </span>}
-                        >
-                            <Menu.Item key="3"> Modulo 1   <Icon type="unlock" /><Link to="./matstudy"></Link></Menu.Item>
-                            <Menu.Item key="4" disabled>Modulo 2 <Icon type="lock" /></Menu.Item>
+                            </span>}>
+                            <Menu.Item key="4"> Nível  1   <Icon type="unlock" /><Link to="./matstudy"></Link></Menu.Item>
+                            <Menu.Item key="5" disabled> Nível 2 <Icon type="lock" /></Menu.Item>
+                            <SubMenu
+                                key="sub2"
+                                title={<span disabled><Icon type="lock" />
+                                    <span >Nível 3</span>
+                                </span>} >
+                                <Menu.Item key="6" disabled>Modulo 1   <Icon type="lock" /></Menu.Item>
+                                <Menu.Item key="7" disabled>Modulo 2 <Icon type="lock" /></Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="8" disabled> Nível 4 <Icon type="lock" /></Menu.Item>
                         </SubMenu>
                         <SubMenu
-                            key="sub2"
+                            key="sub3"
                             title={<span><Icon type="area-chart" />
                                 <span>Relatórios</span>
-                            </span>}
-                        >
-                            <Menu.Item key="5">Modulo 1   <Icon type="unlock" /> <Link to="./Report"></Link></Menu.Item>
-                            <Menu.Item key="6" disabled>Modulo 2 <Icon type="lock" /></Menu.Item>
+                            </span>} >
+                            <Menu.Item key="8"> Nível 1   <Icon type="unlock" /> <Link to="./Report"></Link></Menu.Item>
+                            <Menu.Item key="9" disabled> Nível 2 <Icon type="lock" /></Menu.Item>
+                            <SubMenu
+                                key="sub2"
+                                title={<span disabled><Icon type="lock" />
+                                    <span >Nível 3</span>
+                                </span>} >
+                                <Menu.Item key="10" disabled>Modulo 1   <Icon type="lock" /></Menu.Item>
+                                <Menu.Item key="11" disabled>Modulo 2 <Icon type="lock" /></Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="12" disabled> Nível 4 <Icon type="lock" /></Menu.Item>
                         </SubMenu>
-                        <Menu.Item key="7">
+                        <Menu.Item key="13">
                             <Icon type="question" />
                             <span>Ajuda</span>
                         </Menu.Item>
                     </Menu>
                 </Sider>
                 <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }} />
+                    <Header style={{ background: '#fff', padding: 0 }} >
+                        dala
+                    </Header>
+
                     <Content style={{ margin: '0 16px' }}>
                         <div>
                             <Menu />
@@ -84,7 +102,7 @@ export default class Primary extends React.Component {
                         </div>
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>
-                        NEES-Núcleo de Excelência em Tecnologias Sociais 2019
+                        Copyright 2019- ECG Tutor
           </Footer>
                 </Layout>
             </Layout>
--- src/components/tree.js
+++ src/components/tree.js
@@ -1,22 +1,80 @@
 import React from 'react';
-import { Steps, Progress, Timeline } from 'antd';
+import PropTypes from 'prop-types';
+import { Progress } from 'antd';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const Step = Steps.Step;
+const styles = {
+    root: { 
+        width:500,
+    },
+};
 
-
-export default class Tree extends React.Component {
-    render() {
-        return (
-
-            <div>
-                <Progress type="circle" percent={100} />
-                <Progress type="circle" percent={20} />
-                <Progress type="circle" percent={0} />
-
-            </div>
-
-
-        );
-    }
-
-}
+function PositionedTooltips(props) {
+    const { classes } = props;
+    return (
+      <div className={classes.root} controls style={{ marginLeft: '275px' }}>
+        <Grid container justify="center">
+          <Grid item>
+            
+          <Progress type="circle" percent={100} />
+            
+            
+          </Grid>
+        </Grid>
+        <Grid container justify="center">
+          <Grid item xs={6}>
+            
+          <Progress type="circle" percent={0} />
+           
+            <br />
+            
+              <Button>left</Button>
+           
+            <br />
+           
+              <Button>left-end</Button>
+            
+          </Grid>
+          <Grid item container xs={6} alignItems="flex-end" direction="column" spacing={0}>
+            <Grid item>
+              
+            <Progress type="circle" percent={0} />
+              
+            </Grid>
+            <Grid item>
+              
+                <Button>right</Button>
+             
+            </Grid>
+            <Grid item>
+              
+                <Button>right-end</Button>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container justify="center">
+          <Grid item>
+            
+              <Button>bottom-start</Button>
+           
+            
+              <Button>bottom</Button>
+           
+            
+              <Button>bottom-end</Button>
+           
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+  
+  PositionedTooltips.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(PositionedTooltips);
