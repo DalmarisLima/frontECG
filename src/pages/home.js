0a83b98d882df5481e8484bbfc9e6c9036f3fd28
unknown <moraesdalmarislima@gmail.com>
2019-05-31 23:47:38 -0300

    commit inf

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import { Card, Col, Row } from 'antd';
-import Coresl from '../components/carosel';
+import { Card, Col, Row, Popover } from 'antd';
 import Feed from '../components/feed';
+import Paragraph from 'antd/lib/typography/Paragraph';
+import cora from './cora1.svg';
+
+
+const text = <span>Olá! Eu sou a Cora e estou aqui para ajudar a navegar no ECG Tutor.</span>;
+const content = (
+    <Paragraph strong={true}>
+        <img src={cora} className="Cora" alt="cora" />
+        <Paragraph strong={true}>
+            <p> O ECG tutor é um sistema com inteligência artificial</p>
+            <p> capaz de individualizar seu aprendizado,</p>
+            <p>além de conter tecnologia de gamificação,</p>
+            <p>para que você se sinta engajado e motivado com seus estudos.</p>
+            <p>O curso é composto por etapas, sendo possível desbloquear a </p>
+            <p>próxima etapa apenas após concluir a anterior.</p>
+            <p>Antes de iniciar a primeira fase, você deve escolher um Avatar. </p>
+            <p>Vamos lá? É só clicar na área de perfil do usuário no canto </p>
+            <p>superior direito da tela.</p>
+            <p>Inicialmente, seu Avatar ainda é um bebê, </p>
+            <p>mas poderá evoluir após a conclusão da primeira fase.</p>
+        </Paragraph>
+    </Paragraph>
+
+);
 
 const tablis = [{
-    key: 'tab1',
-    tab: 'Cora'
-}, {
     key: 'tab2',
     tab: 'Feed Notícias',
 
 }];
 
 const contentList = {
-    tab1: <div>
-<Coresl/>
-    </div>,
     tab2: <div><Feed />
     </div>,
 };
 
 export default class Home extends React.Component {
     state = {
-        key: 'tab1',
+        key: 'tab2',
     }
 
     onTabChange = (key, type) => {
@@ -33,6 +50,7 @@ export default class Home extends React.Component {
     render() {
         return (
             <div>
+                <Popover placement="top" content={content} title={text}>
                 <Card
                     style={{ width: '100%', marginTop: "70px" }}
                     title='Início'
@@ -41,6 +59,7 @@ export default class Home extends React.Component {
                     onTabChange={(key) => { this.onTabChange(key, 'key'); }}>
                     {contentList[this.state.key]}
                 </Card>
+                </Popover>
             </div>
         );
     }
