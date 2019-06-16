9d25773761e0b3620931385a32057d01964b4123
unknown <moraesdalmarislima@gmail.com>
2019-06-16 20:04:53 -0300

    perguntas

--- src/components/p1.js
+++ src/components/p1.js
@@ -1,115 +1,76 @@
 import React from "react";
 import {
+  Steps,
+  Button,
+  message,
   Radio,
   Alert,
   Row,
   Col,
-  Button,
   notification,
   Card,
-  Avatar,
+  Modal,
   Typography
 } from "antd";
 
 const RadioGroup = Radio.Group;
-const RadioButton = Radio.Button;
-const { Title } = Typography;
-
+const { Step } = Steps;
 const close = () => {
   console.log(
     "Notification was closed. Either the close button was clicked or duration time elapsed."
   );
 };
-const openNotification3 = () => {
-  const key = `open${Date.now()}`;
-  const btn = (
-    <div>
-      <p>
+
+function info() {
+  Modal.info({
+    title: 'Lamento, resposta incoreta',
+    content: (
+      <div>
+        <p>Aqui vai a dica </p>
+      </div>
+    ),
+    onOk() { },
+  });
+}
+
+function success() {
+  Modal.success({
+    title: 'Parabés! Resposta Correta!',
+    onOk() { },
+  });
+}
+
+function success2() {
+
+  Modal.success({
+    content: (
+      <div>
         <strong>
           Parabéns!! Seu avatar evoluiu, você agora pode escolher entre o avatar
           de Doutor(a), basta que o clique
-        </strong>
-      </p>
-    </div>
-  );
-  notification.open({
-    message: "Muito Bem!",
-    description: <Alert message="Resposta Correta" type="success" />,
-    btn,
-    key,
-    onClose: close,
-    icon: <Avatar src="https://img.icons8.com/dusk/64/000000/ok.png" />
+            </strong>
+      </div>
+
+    ),
+    next() {
+      const current = this.state.current + 1;
+      this.setState({ current });
+    },
+    onOk() { },
   });
-};
+}
 
-const openNotification = () => {
-  const key = `open${Date.now()}`;
-  const btn = (
-    <Button type="default" size="small" onClick={() => notification.close(key)}>
-      Próxima Pergunta
-    </Button> 
-  );
-  notification.open({
-    message: "Muito Bem!",
-    description: <Alert message="Resposta Correta" type="success" />,
-    btn,
-    key,
-    onClose: close,
-    icon: <Avatar src="https://img.icons8.com/dusk/64/000000/ok.png" />
-  });
-};
-const openNotificationError = () => {
-  const key = `open${Date.now()}`;
-  const btn = (
-    <Button type="default" size="small" onClick={() => notification.close(key)}>
-      Dica
-    </Button>
-  );
-  notification.open({
-    message: "Lamento!",
-    description: <Alert message="Resposta Incorreta" type="error" />,
-    btn,
-    key,
-    onClose: close,
-    icon: <Avatar src="https://img.icons8.com/dusk/64/000000/cancel.png" />
-  });
-};
-
-
-export default class Pratice extends React.Component {
-  state = {
-    value: null,
-    noTitleKey: "p1"
-  };
-  onChange = e => {
-    console.log("radio checked", e.target.value);
-    this.setState({
-      value: e.target.value
-    });
-  };
-  onTabChange = (key, type) => {
-    console.log(key, type);
-    this.setState({ [type]: key });
-  };
-
-  render() {
-    return (
-      <div>
-        <Card
-          style={{ width: "100%" }}
-          activeTabKey={this.state.noTitleKey}
-          onTabChange={key => {
-            this.onTabChange(key, "noTitleKey");
-          }}
-        >
-          <div>
+const steps = [
+  {
+    title: 'Pergunta 1',
+    content: <div>
       <p>
         (MUNDO EDUCAÇÃO – UOL) Para que o coração mantenha seu ritmo, existe um
         complexo sistema excitatório atuando nesse órgão. Uma dessas estruturas
         é conhecida como marcapasso natural, que atua mandando impulsos que
         garantem a contração do coração. Verifique as alternativas a seguir e
         marque aquela que indica corretamente o nome dessa região do coração.
-      </p>
+    </p>
       <Row>
         <Col span={24}>
           <RadioGroup buttonStyle="solid">
@@ -117,47 +78,178 @@ export default class Pratice extends React.Component {
               <Col span={24} style={{ marginTop: "15px" }}>
                 <Button
                   type="default"
-                  onClick={
-                    (notification.close(openNotificationError.key),
-                    openNotificationError)
-                  }
-                >
+                  onClick={info}>
                   A
-                </Button>
+              </Button>
                 Miocárdio.
-              </Col>
+            </Col>
 
               <Col span={24} style={{ marginTop: "15px" }}>
-                <Button type="default" onClick={openNotificationError}>
+                <Button type="default" onClick={info}>
                   B
-                </Button>
+              </Button>
                 Valva pulmonar.
-              </Col>
+            </Col>
               <Col span={24} style={{ marginTop: "15px" }}>
-                <Button type="default" onClick={openNotification}>
+                <Button type="default" onClick={success}>
                   C
-                </Button>
+              </Button>
                 Nó sinoatrial.
-              </Col>
+            </Col>
 
               <Col span={24} style={{ marginTop: "15px" }}>
-                <Button type="default" onClick={openNotificationError}>
+                <Button type="default" onClick={info}>
                   D
-                </Button>
+              </Button>
                 Septo interatrial.
-              </Col>
+            </Col>
               <Col span={24} style={{ marginTop: "15px" }}>
-                <Button type="default" onClick={openNotificationError}>
+                <Button type="default" onClick={info}>
                   E
-                </Button>
+              </Button>
                 Endocárdio.
-              </Col>
+            </Col>
             </Row>
           </RadioGroup>
         </Col>
       </Row>
     </div>
-        </Card>
+  },
+  {
+    title: 'Pergunta 2',
+    content: <div>
+      <p>
+        (CESGRANRIO- 2008) São células de tecidos que demonstram propriedade de
+        despolarização diastólica espontânea em corações normais, EXCETO:
+    </p>
+      <Row>
+        <Col span={24}>
+          <RadioGroup buttonStyle="solid">
+            <Row>
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  A
+              </Button>
+                Feixe de His.
+            </Col>
+
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  B
+              </Button>
+                Fibras de Purkinje.
+            </Col>
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  C
+              </Button>
+                Nódulo Sinoatrial.
+            </Col>
+
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={success}>
+                </Button>
+                Células musculares miocárdicas.
+            </Col>
+
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  E
+              </Button>
+                Ramos direito e esquerdo.
+            </Col>
+            </Row>
+          </RadioGroup>
+        </Col>
+      </Row>
+    </div>
+  },
+  {
+    title: 'Pergunta 3',
+    content: <div>
+      <p>
+        (MEDELETRO VOL 1) A sequência normal da ativação ao longo do tecido de
+        condução cardíaco é melhor representada por:
+    </p>
+      <Row>
+        <Col span={24}>
+          <RadioGroup buttonStyle="solid">
+            <Row>
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  A
+              </Button>
+                Nodo atrioventricular – Nodo sinusal – Feixe de His – Fibras de
+                Purkinje.
+            </Col>
+
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={success2}>
+                  B
+              </Button>
+                Nodo sinusal – Feixes internodais – Nodo Atrioventricular –
+                Feixe de His – Sistema de Purkinje.
+            </Col>
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  C
+              </Button>
+                Nodo sinoatrial – Nodo atrioventricular – Sistema de Purkinje –
+                Feixe de His.
+            </Col>
+
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  D
+              </Button>
+                Feixe de His – Nodo atrioventricular – Nodo sinusal – Feixe de
+                His – Sistema de Purkinje.
+            </Col>
+
+              <Col span={24} style={{ marginTop: "15px" }}>
+                <Button type="default" onClick={info}>
+                  E
+              </Button>
+                Nodo atrioventricular – Feixes interatriais – Nodo sinusal –
+                Feixe de His – Sistema de Purkinje.
+            </Col>
+            </Row>
+          </RadioGroup>
+        </Col>
+      </Row>
+    </div>
+  },
+];
+
+export default class Ques extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0,
+    };
+  }
+
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+  render() {
+    const { current } = this.state;
+    return (
+      <div>
+        <Steps current={current}>
+          {steps.map(item => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => this.next()}>
+              Próxima Pergunta
+            </Button>
+          )}
+        </div>
       </div>
     );
   }
