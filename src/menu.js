6c3390223b1e01e4f07f7bc1a149cad42fa68865
50victor <victorvcdb@gmail.com>
2019-07-08 20:30:51 -0300

    commit 08/07

---
+++ src/menu.js
@@ -0,0 +1,131 @@
+import React from "react";
+import { Layout, Menu, Icon, Typography, Badge } from "antd";
+import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Report from "../pages/report";
+import Home from "../pages/home";
+import StudyMaterial from "../pages/studyMaterial";
+import Ranking from "../pages/ranking";
+import User from "../components/user";
+import Avaliation from "../pages/avaliation";
+import Evolution from "../components/evolution";
+
+const { Header, Content, Footer } = Layout;
+const SubMenu = Menu.SubMenu;
+
+export default class Primary extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            style={{ lineHeight: "64px" }}
+          >
+            <Menu.Item key="2">
+              <Icon type="home" style={{ fontSize: "20px" }} />
+              <span>Início</span>
+              <Link to="./" />
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="trophy" style={{ fontSize: "20px" }} />
+              <span>Ranking</span>
+              <Link to="./Ranking" />
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  {" "}
+                  <Icon type="snippets" style={{ fontSize: "20px" }} />
+                  <span>Material de Estudos</span>
+                </span>
+              }
+            >
+              <Menu.Item key="4">
+                <Icon type="file-text" /> Nível 1 <Link to="./matstudy" />
+              </Menu.Item>
+              <Menu.Item key="5" disabled>
+                {" "}
+                <Icon type="lock" />
+                Nível 2
+              </Menu.Item>
+              <Menu.Item key="6" disabled>
+                <Icon type="lock" /> Nível 3{" "}
+              </Menu.Item>
+              <Menu.Item key="8" disabled>
+                <Icon type="lock" /> Nível 4{" "}
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3"
+              title={
+                <span>
+                  <Icon type="area-chart" style={{ fontSize: "20px" }} />
+                  <span>Relatórios</span>
+                </span>
+              }
+            >
+              <Menu.Item key="8">
+                <Icon type="area-chart" /> Nível 1 <Link to="./Report" />
+              </Menu.Item>
+              <Menu.Item key="9" disabled>
+                {" "}
+                <Icon type="lock" /> Nível 2
+              </Menu.Item>
+              <Menu.Item key="6" disabled>
+                <Icon type="lock" /> Nível 3{" "}
+              </Menu.Item>
+              <Menu.Item key="12" disabled>
+                <Icon type="lock" /> Nível 4{" "}
+              </Menu.Item>
+            </SubMenu>
+            <Menu.Item key="13">
+              <Icon type="star" style={{ fontSize: "20px" }} />
+              <span>Avaliar</span>
+              <Link to="./Avaliation" />
+            </Menu.Item>
+            <SubMenu
+              key="sub14"
+              title={
+                <span>
+                  {" "}
+                  <User />
+                </span>
+              }
+            >
+              <Menu.Item key="14">
+                <Icon type="user" /> Perfil
+              </Menu.Item>
+              <Menu.Item key="15">
+                <Badge dot>
+                  <Icon type="notification" />
+                </Badge>{" "}
+                Notificações{" "}
+              </Menu.Item>
+              <Menu.Item key="16">
+                <Icon type="logout" /> Sair
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Header>
+        <Content style={{ margin: "50px" }}>
+          <div>
+            <Menu />
+            <Route path="/" exact component={Home} />
+            <Route path="/ranking" exact component={Ranking} />
+            <Route path="/report" exact component={Report} />
+            <Route path="/matstudy" exact component={StudyMaterial} />
+            <Route path="/avaliation" exact component={Avaliation} />
+          </div>
+        </Content>
+
+        <Footer style={{ textAlign: "center" }}>
+          Copyright ©2019 ECG TUTOR
+        </Footer>
+      </Layout>
+    );
+  }
+}
---
+++ src/p1.js
@@ -0,0 +1,283 @@
+import React from "react";
+import {
+  Steps,
+  Button,
+  message,
+  Radio,
+  Alert,
+  Row,
+  Col,
+  notification,
+  Card,
+  Modal,
+  Typography
+} from "antd";
+import Evol from "../components/evolution";
+
+const RadioGroup = Radio.Group;
+const { Step } = Steps;
+const close = () => {
+  console.log(
+    "Notification was closed. Either the close button was clicked or duration time elapsed."
+  );
+};
+
+const dica = () => {
+  message.success("Letra C", 1);
+};
+const dica2 = () => {
+  message.success("Letra D", 1);
+};
+const dica3 = () => {
+  message.success("Letra B", 1);
+};
+
+function info() {
+  Modal.info({
+    title: "Lamento, resposta incorreta",
+    content: <Button onClick={dica}>Dica</Button>,
+    onOk() {}
+  });
+}
+function info2() {
+  Modal.info({
+    title: "Lamento, resposta incorreta",
+    content: <Button onClick={dica2}>Dica</Button>,
+    onOk() {}
+  });
+}
+
+function info3() {
+  Modal.info({
+    title: "Lamento, resposta incorreta",
+    content: <Button onClick={dica3}>Dica</Button>,
+    onOk() {}
+  });
+}
+
+function success() {
+  Modal.success({
+    title: "Parabéns! Resposta Correta!",
+    onOk() {}
+  });
+}
+
+function success2() {
+  Modal.success({
+    content: (
+      <div>
+        <Evol />
+        <strong>
+          Parabéns!! Seu avatar evoluiu, você agora pode escolher entre o avatar
+          de Doutor(a), basta que o clique
+        </strong>
+      </div>
+    ),
+    next() {
+      const current = this.state.current + 1;
+      this.setState({ current });
+    },
+    onOk() {}
+  });
+}
+
+const steps = [
+  {
+    title: "Pergunta 1",
+    content: (
+      <div>
+        <p>
+          (MUNDO EDUCAÇÃO – UOL) Para que o coração mantenha seu ritmo, existe
+          um complexo sistema excitatório atuando nesse órgão. Uma dessas
+          estruturas é conhecida como marcapasso natural, que atua mandando
+          impulsos que garantem a contração do coração. Verifique as
+          alternativas a seguir e marque aquela que indica corretamente o nome
+          dessa região do coração.
+        </p>
+        <Row>
+          <Col span={24}>
+            <RadioGroup buttonStyle="solid">
+              <Row>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info}>
+                    A
+                  </Button>
+                  Miocárdio.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info}>
+                    B
+                  </Button>
+                  Valva pulmonar.
+                </Col>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={success}>
+                    C
+                  </Button>
+                  Nó sinoatrial.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info}>
+                    D
+                  </Button>
+                  Septo interatrial.
+                </Col>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info}>
+                    E
+                  </Button>
+                  Endocárdio.
+                </Col>
+              </Row>
+            </RadioGroup>
+          </Col>
+        </Row>
+      </div>
+    )
+  },
+  {
+    title: "Pergunta 2",
+    content: (
+      <div>
+        <p>
+          (CESGRANRIO- 2008) São células de tecidos que demonstram propriedade
+          de despolarização diastólica espontânea em corações normais, EXCETO:
+        </p>
+        <Row>
+          <Col span={24}>
+            <RadioGroup buttonStyle="solid">
+              <Row>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info2}>
+                    A
+                  </Button>
+                  Feixe de His.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info2}>
+                    B
+                  </Button>
+                  Fibras de Purkinje.
+                </Col>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info2}>
+                    C
+                  </Button>
+                  Nódulo Sinoatrial.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={success}>
+                    D
+                  </Button>
+                  Células musculares miocárdicas.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info2}>
+                    E
+                  </Button>
+                  Ramos direito e esquerdo.
+                </Col>
+              </Row>
+            </RadioGroup>
+          </Col>
+        </Row>
+      </div>
+    )
+  },
+  {
+    title: "Pergunta 3",
+    content: (
+      <div>
+        <p>
+          (MEDELETRO VOL 1) A sequência normal da ativação ao longo do tecido de
+          condução cardíaco é melhor representada por:
+        </p>
+        <Row>
+          <Col span={24}>
+            <RadioGroup buttonStyle="solid">
+              <Row>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info3}>
+                    A
+                  </Button>
+                  Nodo atrioventricular – Nodo sinusal – Feixe de His – Fibras
+                  de Purkinje.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={success2}>
+                    B
+                  </Button>
+                  Nodo sinusal – Feixes internodais – Nodo Atrioventricular –
+                  Feixe de His – Sistema de Purkinje.
+                </Col>
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info3}>
+                    C
+                  </Button>
+                  Nodo sinoatrial – Nodo atrioventricular – Sistema de Purkinje
+                  – Feixe de His.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info3}>
+                    D
+                  </Button>
+                  Feixe de His – Nodo atrioventricular – Nodo sinusal – Feixe de
+                  His – Sistema de Purkinje.
+                </Col>
+
+                <Col span={24} style={{ marginTop: "15px" }}>
+                  <Button type="default" onClick={info3}>
+                    E
+                  </Button>
+                  Nodo atrioventricular – Feixes interatriais – Nodo sinusal –
+                  Feixe de His – Sistema de Purkinje.
+                </Col>
+              </Row>
+            </RadioGroup>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+];
+
+export default class Ques extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0
+    };
+  }
+
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+  render() {
+    const { current } = this.state;
+    return (
+      <div>
+        <Steps current={current}>
+          {steps.map(item => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => this.next()}>
+              Próxima Pergunta
+            </Button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
