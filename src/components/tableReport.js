e8deb9c64628c57f9c9d6261f69e9df240de7409
unknown <moraesdalmarislima@gmail.com>
2019-08-20 22:52:19 -0300

    commit table

--- src/components/tableReport.js
+++ src/components/tableReport.js
@@ -36,7 +36,47 @@ const data = [{
     video: <div>12 Lead ECG Explained, Animation <Progress type="circle" percent={50} width={30} size={"small"} /></div>,
     praticar: <div>Praticar Nível 2 <Progress type="circle" percent={70} width={30} size={"small"} /></div>,
     caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={50} width={30} size={"small"} /></div>,
-}];
+},
+{
+    key: '2',
+    texto: <div>Colaboração dos eltrodos no paciente<Progress type="circle" percent={0} width={30} size={"small"} /> </div>,
+    video: <div>Colaboração dos eltrodos no paciente<Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    praticar: <div>Praticar Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+},
+{
+    key: '3',
+    texto: <div>Papel de ECG, tempo e voltagem <Progress type="circle" percent={0} width={30} size={"small"} /> </div>,
+    video: <div>Papel de ECG, tempo e voltagem <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    praticar: <div>Praticar Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+},
+{
+    key: '4',
+    texto: <div>Traçado eletrocardiográfico <Progress type="circle" percent={0} width={30} size={"small"} /> </div>,
+    video: <div>Traçado eletrocardiográfico <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    praticar: <div>Praticar Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+},{
+    key: '5',
+    texto: <div>Eixo Eletrico <Progress type="circle" percent={0} width={30} size={"small"} /> </div>,
+    video: <div>Eixo Eletrico <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    praticar: <div>Praticar Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+},{
+    key: '6',
+    texto: <div>Ritmo <Progress type="circle" percent={0} width={30} size={"small"} /> </div>,
+    video: <div>Ritmo<Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    praticar: <div>Praticar Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+},{
+    key: '7',
+    texto: <div>Frequência Cardiaca<Progress type="circle" percent={0} width={30} size={"small"} /> </div>,
+    video: <div>Frequência Cardiaca<Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    praticar: <div>Praticar Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+    caso: <div>Caso Clínico Nível 2 <Progress type="circle" percent={0} width={30} size={"small"} /></div>,
+},
+];
 export default class Tabela extends React.Component {
     render() {
         return (
